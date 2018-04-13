2ab49d51f417f7f1ae3472f520eedd341474220d
vydhi-v <ydswaran@gmail.com>
2018-04-12 20:21:41 -0400

    added auth, verify and logout

--- app.js
+++ app.js
@@ -1,13 +1,47 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const passport = require('passport');
+const session = require('express-session');
+const cookieParse = require('cookie-parser');
 const app = express();
+
+//Load User model
+require('./models/UserModel');
 
 //Passport config
 require('./config/passport')(passport);
 
 //Load routes
 const auth = require('./routes/auth');
+
+//Load keys
+const keys = require('./config/keys');
+
+//Map global promises
+mongoose.Promise = global.Promise;
+
+//Mongoose connect
+mongoose.connect(keys.mongoURI)
+         .then(()=> console.log('MongoDB connected'))
+         .catch(err => console.log(err));
+
+         //express-session middleware
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false
+}));
+
+//Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Set globals
+app.use((req,res,next)=>{
+    res.locals.user = req.user || null;
+    next();
+});
+
 
 app.get('/',(req,res) => {
     res.send('Welcome to Storybooks!');
--- routes/auth.js
+++ routes/auth.js
@@ -3,8 +3,30 @@ const router = express.Router();
 const passport = require('passport');
 
 //Controller for /auth routes
-router.get('/google', passport.authenticate('google',
-    {scope:['profile', 'email']}
-));
+
+//Auth
+router.get('/google', passport.authenticate('google', {scope:['profile', 'email']}));
+
+//Auth Callback
+router.get('/google/callback',
+        passport.authenticate('google', {failureRedirect:'/'}),
+        //Successful authentication, redirect home
+        (req,res)=>{
+            res.redirect('/dashboard');
+        }
+);
+
+router.get('/verify',(req,res) => {
+    if(req.user){
+        console.log(req.user);
+    }else{
+        console.log('Not Auth');
+    }
+});
+
+router.get('/logout', (req,res)=>{
+    req.logout();
+    res.redirect('/');
+})
 
 module.exports = router;
